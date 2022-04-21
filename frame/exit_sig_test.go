b4b8a757f690e5de0f57dc737e5f979d1e72422a
崔浩鑫 <cuihaoxin@cuihaoxindeMacBook-Pro.local>
2022-04-21 11:16:58 +0800

    feat(add exit_sig): add exit_sig

---
+++ frame/exit_sig_test.go
@@ -0,0 +1,42 @@
+package frame
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestExitSig(t *testing.T) {
+	exitSig := New(func() error {
+		fmt.Println("hello,world")
+		return nil
+	})
+
+	fmt.Println("start")
+
+	exitSig.RunGo(func(exitSig <-chan int) {
+		for {
+			select {
+			case <-exitSig:
+				fmt.Println("close goroutine 1")
+				return
+			default:
+				time.Sleep(3 * time.Second)
+				fmt.Println("work 1")
+			}
+		}
+	})
+
+	exitSig.RunGo(func(exitSig <-chan int) {
+		<-exitSig
+		fmt.Println("close goroutine 2")
+	})
+
+	exitSig.RunGo(func(exitSig <-chan int) {
+		<-exitSig
+		fmt.Println("close goroutine 3")
+	})
+
+	time.Sleep(3 * time.Second)
+	exitSig.Close()
+}
